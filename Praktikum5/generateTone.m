3be04441d1a02ee92858e9cd332cf117434e37c0
Michael Busch <b.michael.2002@web.de>
2024-08-10 16:45:49 +0200

    Ear Rape

---
+++ Praktikum5/generateTone.m
@@ -0,0 +1,16 @@
+function x = generateTone(fn, Td)
+    global Fs;
+    global c_m;
+    t = 0: 1/(Fs - 1) :Td;
+
+    w_n = 2 * pi * fn;
+    w_0 = w_n / 2;
+
+    m = [1, 2, 3, 4, 6, 8, 10, 12, 14, 16, 18, 20, 24];
+
+    % Signal generieren
+    x = zeros(size(t));
+    for k = 1:length(m)
+        x = x + c_m(1,k) * sin(m(k) * w_0 * t);
+    end
+end
