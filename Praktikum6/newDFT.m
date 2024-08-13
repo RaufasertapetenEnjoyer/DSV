7dc7ecdce7686a813ecc624ffd70f86b2e25b62f
Michael Busch <b.michael.2002@web.de>
2024-08-13 14:43:00 +0200

    Praktikum 6 soweit fertig
    ToDo: Pr 5 Rechtecksspannung

--- Praktikum6/newDFT.m
+++ Praktikum6/newDFT.m
@@ -1,12 +1,12 @@
 function [X] = newDFT(x, key_min, key_max)
 N = length(x);
-X = zeros(1,key_max - key_min + 1);
+X = zeros(1,key_max - key_min + 1); % X besitzt nur so viele Werte wie Tasten zwischen key_min und key_max sind
 
-for k = key_min : key_max
-    m = calculateFrequency(440, k);
-    i = k - key_min;
+for k = key_min : key_max % aktuelle Taste
+    m = calculateFrequency(440, k); % Berechnung der Frequenz
+    i = k - key_min; % Index i bei 0 beginnend zum Befüllen vom Vektor X
     for n = 0 : N-1
-        X(1, i + 1) = X(1, i + 1) + x(1, n + 1) .* exp(-1.*1i.*2.*pi.*((m.*n)./N));
+        X(1, i + 1) = X(1, i + 1) + x(1, n + 1) .* exp(-1.*1i.*2.*pi.*((m.*n)./N)); % Formel allgemein unverändert
     end
 end
 end
